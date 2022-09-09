caecdb0e856ab68a9b74ad734a6f3b9231ff0a1a
Sevencito07 <108514442+Sevencito07@users.noreply.github.com>
2022-09-09 01:42:21 -0500

    Update Client.js

--- src/supabase/Client.js
+++ src/supabase/Client.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js"
 
 
  export const supabase =    createClient(
-   'https://owapcnevgbxibipugbys.supabase.co',
-   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93YXBjbmV2Z2J4aWJpcHVnYnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE2NTU4MjIsImV4cCI6MTk3NzIzMTgyMn0.IxALztANC_NfkbNYEgXnIkQbQ4v680Vr6yZUNH0Njgk'
+   'https://dccjkalkmnevugSCSDCSDCC',
+   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSscsdjvshd2J4aWJpcHVnYnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE2NTU4MjIsImV4cCI6MTk3NzIzMTgyMn0.IxALztANC_NfkbNYEgXnIkQbQ4v680Vr6yZUNH0Njgk'
     )
